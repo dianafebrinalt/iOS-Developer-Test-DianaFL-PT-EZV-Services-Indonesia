4d1d2e17857b63419e72daf64fd70f1b00a06b3d
dianafebrinalt <febrinalt123@gmail.com>
2023-03-18 11:17:53 +0700

    - create and modify UI for Detail Page - add new controller for detail page

---
+++ iOS-Developer-Test-DianaFL-PT-EZV-Services-Indonesia/Controller/DetailProductController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailProductController.swift
+//  iOS-Developer-Test-DianaFL-PT-EZV-Services-Indonesia
+//
+//  Created by Diana Febrina Lumbantoruan on 18/03/23.
+//
+
+import UIKit
+
+class DetailProductController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
