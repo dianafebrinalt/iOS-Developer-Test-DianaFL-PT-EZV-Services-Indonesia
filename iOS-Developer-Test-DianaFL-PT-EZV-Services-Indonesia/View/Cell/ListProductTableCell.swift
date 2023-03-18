8decb4d53cfde8e6e3d901b892f4e4d6ab43bdec
dianafebrinalt <febrinalt123@gmail.com>
2023-03-18 11:00:39 +0700

    - add variable product - create func to setData list product cell based on data from API

--- iOS-Developer-Test-DianaFL-PT-EZV-Services-Indonesia/View/Cell/ListProductTableCell.swift
+++ iOS-Developer-Test-DianaFL-PT-EZV-Services-Indonesia/View/Cell/ListProductTableCell.swift
@@ -14,6 +14,12 @@ protocol ListProductTableCellDelegate: AnyObject {
 class ListProductTableCell: UITableViewCell {
     private var thumbnailUrl: String = ""
     var delegate : ListProductTableCellDelegate?
+    var product : Product? {
+        didSet {
+            guard let product = product else { return }
+            setCellWithValuesOf(product)
+        }
+    }
 
     @IBOutlet weak var productThumbnail: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
@@ -22,14 +28,52 @@ class ListProductTableCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        setData()
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+        detailButton.addTarget(self, action: #selector(goToDetailPage), for: .touchUpInside)
     }
     
-    func setData() {
-        productThumbnail.image = UIImage(systemName: "pencil")
-        productNameLabel.text = "HP OPPO"
-        productDescriptionLabel.text = "Good condition"
-        detailButton.addTarget(self, action: #selector(goToDetailPage), for: .touchUpInside)
+    func setCellWithValuesOf(_ product : Product) {
+        setDataProduct(productName: product.title, productDescription: product.description, thumbnail: product.thumbnail)
+    }
+
+    private func setDataProduct(productName: String?, productDescription: String?, thumbnail: String?) {
+        productNameLabel.text = productName
+        productDescriptionLabel.text = productDescription
+        
+        guard let thumbnail = thumbnail else {return}
+        thumbnailUrl = thumbnail
+        
+        guard let thumbnailURL = URL(string: thumbnailUrl) else {
+            self.productThumbnail.image = UIImage(systemName: "iphone.gen1")
+            return
+        }
+        
+        // Before we download the image we clear out the old one
+        self.productThumbnail.image = nil
+        
+        getProductThumbnail(url: thumbnailURL)
+    }
+    
+    func getProductThumbnail(url: URL) {
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            if let error = error {
+                print("Error : \(error.localizedDescription)")
+                return
+            }
+
+            guard let data = data else{
+                print("The Data is Empty")
+                return
+            }
+
+            DispatchQueue.main.async{
+                if let image = UIImage(data: data) {
+                    self.productThumbnail.image = image
+                }
+            }
+        }
+        task.resume()
     }
     
     @objc func goToDetailPage() {
